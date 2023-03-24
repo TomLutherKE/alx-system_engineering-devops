efcffead7b4c0da68deedb578639583b13417265
TomLutherKE <tomluther133@gmail.com>
2023-03-24 09:00:23 -0700

    Task 0 done

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# create file with certain requirements
+file { '/tmp/holberton':
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
+
